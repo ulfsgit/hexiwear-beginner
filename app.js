3947c0d29a6540bfe6674a151b420c458a05b503
ulfsgit <ulfsgit@strobergs.net>
2017-04-19 12:24:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function mainController($scope) {
 
 	main.buttonClicked = function() {
 		if (isBluetoothEnabled()) {
-			let options = {	
+			var options = {	
 				filters: [{name: 'HEXIWEAR'}],	
 				optionalServices: ['0000180a-0000-1000-8000-00805f9b34fb']
 			};
